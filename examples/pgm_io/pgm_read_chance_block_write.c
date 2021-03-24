ef6b1b75a1b0825de1d214c122019b6630cdbe71
euzun10 <uzun.ens@gmail.com>
2021-03-24 19:47:06 +0300

    code Standardization

--- examples/pgm_io/pgm_read_chance_block_write.c
+++ examples/pgm_io/pgm_read_chance_block_write.c
@@ -3,7 +3,6 @@
 
 #include <libpgm.h>
 
-// ---------------------------------------------------------------
 struct point_s
 {
 	int x, y; // row, col
@@ -15,68 +14,18 @@ struct block_s
 };
 
 void
-change_image_block_pixel_value(lpgm_t* pgm, struct block_s block, float new_value);
-// ---------------------------------------------------------------
-
-int
-main(int argc, char** argv)
-{
-	// ------  chechk input parameter    ---------
-	if (argc != 2)
-	{
-		fprintf(stderr, "%s(): Usage: [./pgm_read_chance_block_write.out image_file.pgm] \n", __func__);
-		return -1;
-	}
-	// -------------------------------------------
-
-	// ------   read file   ----------------------
-	const char* file_name = argv[1];
-	fprintf(stderr, "%s(): Reading pgm file: [%s] \n", __func__, file_name);
-
-	lpgm_t pgm;
-	if (pgm_read(file_name, &pgm) != 0)
-	{
-		fprintf(stderr, "%s(): Exiting .. \n", __func__);
-		return -1;
-	}
-	// -------------------------------------------
-
-	// ------    change block pixel value   ------
-	const int new_value = 10;
-	struct block_s block;
-	block.point[0].x = 0;
-	block.point[0].y = 0;
-	block.point[1].x = 100;
-	block.point[1].y = 100;
-
-	change_image_block_pixel_value(&pgm, block, new_value);
-	// -------------------------------------------
-
-	// -------  write_image ---------------------
-	const char* out_file_name = "output.pgm";
-	if (pgm_write(&pgm, out_file_name) != 0)
-	{
-		fprintf(stderr, "%s(): Exiting .. \n", __func__);
-		return -1;
-	}
-	fprintf(stdout, "%s(): Saved pgm file: [%s]\n", __func__, out_file_name);
-	// -------------------------------------------
-
-	pgm_destroy(&pgm);
-
-	return 0;
-}
-
-void
 change_image_block_pixel_value(lpgm_t* pgm, struct block_s block, float new_value)
 {
-	int rows = pgm->im.w;
-	int cols = pgm->im.h;
-	float* data_ptr = pgm->im.data;
+	int i, j, rows, cols;
+	float* data_ptr;
 
-	for (int i = 0; i < rows; ++i)
+	rows = pgm->im.w;
+	cols = pgm->im.h;
+	data_ptr = pgm->im.data;
+
+	for (i = 0; i < rows; ++i)
 	{
-		for (int j = 0; j < cols; ++j)
+		for (j = 0; j < cols; ++j)
 		{
 			if (i >= block.point[0].x && i < block.point[1].x && j >= block.point[0].y && j < block.point[1].y)
 			{
@@ -85,4 +34,47 @@ change_image_block_pixel_value(lpgm_t* pgm, struct block_s block, float new_valu
 			++data_ptr;
 		}
 	}
+}
+
+int
+main(int argc, char** argv)
+{
+	char* input_file_name;
+	const char* out_file_name = "output.pgm";
+	lpgm_t pgm;
+	int new_value;
+	struct block_s block;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "%s(): Usage: [./pgm_read_chance_block_write.out image_file.pgm] \n", __func__);
+		return -1;
+	}
+
+	input_file_name = argv[1];
+	fprintf(stderr, "%s(): Reading pgm file: [%s] \n", __func__, input_file_name);
+	if (lpgm_file_read(input_file_name, &pgm) != LPGM_OK)
+	{
+		fprintf(stderr, "%s(): Exiting .. \n", __func__);
+		return -1;
+	}
+
+	// change block pixel value
+	block.point[0].x = 0;
+	block.point[0].y = 0;
+	block.point[1].x = 100;
+	block.point[1].y = 100;
+	new_value = 10;
+	change_image_block_pixel_value(&pgm, block, new_value);
+
+	if (lpgm_file_write(&pgm, out_file_name) != 0)
+	{
+		fprintf(stderr, "%s(): Exiting .. \n", __func__);
+		return -1;
+	}
+	fprintf(stdout, "%s(): Saved pgm file: [%s] \n", __func__, out_file_name);
+
+	lpgm_file_destroy(&pgm);
+
+	return 0;
 }
